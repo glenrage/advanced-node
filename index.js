d68f6450649c069e4b0ec1f2d87957cf1fdaaae5
glenrage <phamgalang@gmail.com>
2018-04-13 16:16:10 -0700

    added node clustering

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const app = express();
+const cluster = require('cluster');
+const crypto = require('crypto');
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1;
 
-function doWork(duration) {
-  const start = Date.now();
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // cause index.js to be executed *again* but in child mode
 
-  while (Date.now() - start < duration) {}
+  cluster.fork();
+} else {
+  // child code executes
+  const app = express();
+
+  app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      console.log('1:', Date.now() - start);
+      res.send('hi');
+    });
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!');
+  });
+
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('hi');
-});
-
-app.listen(3000);
