3c082a297785e7aa53bedba5a2adc09e4979ab5c
glenrage <phamgalang@gmail.com>
2018-06-26 16:46:05 -0700

    modal #7

--- level2/project/src/App.js
+++ level2/project/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './ToggleRenderProps';
+import Toggle from './ToggleRPC';
+import Portal from './Portal';
+import Modal from './Modal';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Toggle
-          render={({ on, toggle }) => (
-            <div>
-              {on && <h1>Show Me</h1>}
-
-              <button onClick={toggle}> Show / Hide </button>
-            </div>
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              <button onClick={toggle}>Login</button>
+              <Modal on={on} toggle={toggle}>
+                <h1>still in modal</h1>
+              </Modal>
+            </Fragment>
           )}
-        />
+        </Toggle>
       </div>
     );
   }
