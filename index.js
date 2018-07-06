4f3b1f3e468a0fb8844705064266d6ce5e8fe52f
glenrage <phamgalang@gmail.com>
2018-07-05 19:03:57 -0700

    start proj

--- index.js
+++ index.js
@@ -4,25 +4,17 @@ const crypto = require('crypto');
 const start = Date.now();
 process.env.UV_THREADPOOL_SIZE = 1;
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-  // cause index.js to be executed *again* but in child mode
+const app = express();
 
-  cluster.fork();
-} else {
-  // child code executes
-  const app = express();
-
-  app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      console.log('1:', Date.now() - start);
-      res.send('hi');
-    });
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('1:', Date.now() - start);
+    res.send('hi');
   });
+});
 
-  app.get('/fast', (req, res) => {
-    res.send('This was fast!');
-  });
+app.get('/fast', (req, res) => {
+  res.send('This was fast!');
+});
 
-  app.listen(3000);
-}
+app.listen(3000);
