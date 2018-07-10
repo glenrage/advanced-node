07706b886aa0458cf5f191f216d87ebb49d9a22e
glenrage <phamgalang@gmail.com>
2018-07-09 19:05:44 -0700

    security start

--- threads.js
+++ threads.js
@@ -18,6 +18,3 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
   console.log('5:', Date.now() - start);
 });
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-  console.log('6:', Date.now() - start);
-});
